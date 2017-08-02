084796fa8c7c778564000c509c27a32bc7c007d6
Vinge1718 <vinge1718@gmail.com>
2017-08-02 21:15:33 +0300

    Innitialise the two user interface input areas/fields

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  var firstnumber = 1;
+  var secondnumber = parseInt($("#input").val());
+  result = 1;
+
+  var remainder3 = function(secondnumber){
+    return secondnumber%
+  }
+
+/*
+  function pingpong() {
+    $("")
+
+
+    $("#mainwrapper").append(result);
+  }
+*/
+
+
+});
