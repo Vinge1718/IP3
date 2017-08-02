0326cf1b3fea7d2fd0f52bf5a3dcb2053bfd34c5
Vinge1718 <vinge1718@gmail.com>
2017-08-02 23:12:56 +0300

    Add the conditions necessary for testing user number adherence to the divisibility test

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,32 @@
 $(document).ready(function(){
-  var firstnumber = 1;
-  var secondnumber = parseInt($("#input").val());
-  result = 1;
 
-  var remainder3 = function(secondnumber){
-    return secondnumber%
+
+//convert the string into an integer and innitialise the user input number locally i.e within the function "ready"
+
+
+  var userNumber = parseInt($("#input").val());
+
+/*creating a function that is going to test out the user input number
+(Note the use of type-Number input field to control user input data-type)
+*/
+//innitialising the function (a.k.a - naming)
+var divisibilityTest = function(userNumber){
+
+// Multiple Condition testing using the "if...else if"
+
+  if (userNumber % 3 ===0 && userNumber % 5 === 0){
+    return "pingpong";
   }
-
+  else if (userNumber % 3 === 0) {
+    return "ping";
+  }
+  else if (userNumber % 5 === 0) {
+    return "pong";
+  }
+  else {
+    return userNumber;
+  }
+};
 /*
   function pingpong() {
     $("")
