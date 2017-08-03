cf868aa6c2ac5637ab6f1183d29bd6df92056bc1
Vinge1718 <vinge1718@gmail.com>
2017-08-04 00:00:13 +0300

    show comments describing the project flow

--- JS/script.js
+++ JS/script.js
@@ -1,49 +1,79 @@
 $(document).ready(function(){
 
 
-//convert the string into an integer and innitialise the user input number locally i.e within the function "ready"
-
-
-  var userNumber = parseInt($("#input").val());
-
 /*creating a function that is going to test out the user input number
 (Note the use of type-Number input field to control user input data-type)
 */
-//innitialising the function (a.k.a - naming)
-var divisibilityTest = function(userNumber){
+//innitialising/assigning the function a reference term (a.k.a - naming)
+var divisibilityTest = function(i){
 
-// Multiple Condition testing using the "if...else if"
+// Multiple Condition testing using the "if...else if" logic
 
-  if (userNumber % 3 ===0 && userNumber % 5 === 0){
+  if (i % 3 === 0 && i % 5 === 0){ //combined two logic using the operator and --- the other option is to use fifteen since as shown in the alternative logic loop below
     return "pingpong";
   }
-  else if (userNumber % 3 === 0) {
+  else if (i % 3 === 0) {         // remainder text with three
     return "ping";
   }
-  else if (userNumber % 5 === 0) {
+  else if (i % 5 === 0) {         // remainder test with five
     return "pong";
   }
   else {
-    return userNumber;
+    return i;                     // default setting
   }
 };
 
+//using for loop to display the same result
+/*
+----USING "FOR" LOOP TO DISPLAY THE SAME RESULT----- but since it has a "document.write" function
+it over-writes the whole DOM in displaying the result and replaces the page sdt-up with your answer.
+
+var pingPongTest = function(i) {
+ var userNumber = parseFloat($('#userNumber').val());
+   for (i = 1; i <= userNumber; i++) {
+       if (i % 15 === 0) {
+           document.write ("pingpong<br>");
+           continue;
+       };
+
+       if (i % 3 === 0) {
+           document.write ("ping<br>");
+           continue;
+       };
+
+       if (i % 5 === 0) {
+           document.write ("pong<br>");
+           continue;
+       };
+
+       document.write (i + "<br>");
+   }
+};
+*/
 // Creating the storage (memory/array) in which the results/output will be stored as the function above generates them
 
-var output = function(userNumber) {                        //innitialising/ naming the function that is supposed to incrementally
-  var results = [];                                                                 //innitalising an empty array with the name results
+var output = function(input) {                        //innitialising/ naming the function that is supposed to incrementally
+  var results = [];                                   //innitalising an empty array and naming it results (so to access the array we'll ust call the variable "results")
 
-  for (var defaultNumber = 1; defaultNumber <= userNumber; defaultNumber++){
-    results.push(divisibilityTest(defaultNumber));
+  for (var i = 1; i <= input; i++) {
+    results.push(divisibilityTest(i));                // using the push method to add elements to the "results-array" innitialised above through calling the divisibility-test function
   }
-return results;
+return results;                                       // dispaly the now-populated array
 };
 
 
-// prompt the ping pong action from the onlick function imposed on the input button
-$("#button").click(function){
-  var results = output(userNumber);
-  var
-}
+// prompt the ping pong action from the click event imposed on the input button
+$("#button").click(function() {                 // wrapper holding the object describing the functions triggered by the click function.
+  var userNumber = parseInt($("#input").val());   //local innititalisation of the userNumber as the user-entred number
+  var results = output(userNumber);               //innitialising the variable "results" ans assigning it the the "output"function (the function used to populate the array in the previous code block) with the input value fromvthe user as the parameter to use
+  var displayResults = $(".displayResults");      //assigning the the area where the results will be displayed a name (calling it by CLASS - this is to be used later to attach append function)
+  for (var i = 0; i < results.length; i++) {      // loop logic setting the limits to the range of integers that will be be operated on by the above functions
+
+
+    // call the objects in the array and display them (append) as a list item called by class through append-function
+
+    displayResults.append('<li>' + results[i] + '</li>');  //the result[i] access the elements stored in the result array and is then listed through the append function
+  }
+});
 
 });
